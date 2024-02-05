0fe4acff00d797d97ea691db5344733ba9bd36df
Chayut <147202895+Did85258@users.noreply.github.com>
2024-02-05 13:46:23 +0700

    ทำdelete แล้ว

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
+
 require("connect_db.php");
 $conn->select_db("kuyfew");
+
+if (isset($_POST["subDepartment"])) {
+    $fileTmpPath = $_FILES["csvDepartment"]["tmp_name"];
+
+    // เปิดไฟล์ CSV
+    $file = fopen($fileTmpPath, "r");
+    //อ่านแถวแรก
+    fgetcsv($file);
+    // อ่านข้อมูลทีละบรรทัด
+    while (($dataCourse = fgetcsv($file)) !== FALSE) {
+        // เตรียม SQL statement
+        $sql = "INSERT INTO department (
+        departmentNameTh,
+        departmentNameEng) 
+        VALUES ('" . implode("', '", $dataCourse) . "')";
+
+        // ดำเนินการ SQL statement
+        if ($conn->query($sql) === FALSE) {
+            echo "Error: " . $conn->error . "<br>";
+        }
+    }
+    // ปิดไฟล์
+    fclose($file);
+}
+
 //add Courses
 if (isset($_POST["subCourse"])) {
     $fileTmpPath = $_FILES["csvCourse"]["tmp_name"];
@@ -11,15 +37,22 @@ if (isset($_POST["subCourse"])) {
     fgetcsv($file);
     // อ่านข้อมูลทีละบรรทัด
     while (($dataCourse = fgetcsv($file)) !== FALSE) {
+        $departmentNameTh = $dataCourse[0];
+        //ดู nameCourseUse ว่า ตรงกับ courseID ไหน
+        $sql = mysqli_query($conn, "SELECT * FROM department WHERE departmentNameTh= '$departmentNameTh'");
+        if ($row = mysqli_fetch_array($sql)) {
+            $dataCourse[0] = $row['departmentId'];
+
+        }
         // เตรียม SQL statement
-        $sql = "INSERT INTO course (department,
+        $sql = "INSERT INTO course (departmentId,
         nameCourseTh,
         nameCourseUse,
         planCourse ,
         nameCourseEng, 
         nameFullDegreeTh, 
         nameFullDegreeEng,
-        nameInitialsDegreeTh, 
+        nameInitialsDegreeTh , 
         nameInitialsDegreeEng, 
         courseStartYear, 
         courseEndYear, 
@@ -63,7 +96,7 @@ if (isset($_POST["subCategory"])) {
         $nameCourseUse = $data[2];
         //ดู nameCourseUse ว่า ตรงกับ courseID ไหน
         $sql = mysqli_query($conn, "SELECT * FROM course WHERE nameCourseUse= '$nameCourseUse'");
-        if($row = mysqli_fetch_array($sql)) {
+        if ($row = mysqli_fetch_array($sql)) {
             $courseId = $row['courseId'];
 
         }
@@ -95,11 +128,11 @@ if (isset($_POST["subAreas"])) {
     while (($data = fgetcsv($file)) !== FALSE) {
         $subjectAreasNameTh = $data[0];
         $subjectAreasNameEng = $data[1];
-        $categoryNameTh  = $data[2];
+        $categoryNameTh = $data[2];
         //ดู nameCourseUse ว่า ตรงกับ courseID ไหน
         $sql = mysqli_query($conn, "SELECT * FROM category WHERE categoryNameTh= '$categoryNameTh'");
-        if($row = mysqli_fetch_array($sql)) {
-            $categoryId  = $row['categoryId'];
+        if ($row = mysqli_fetch_array($sql)) {
+            $categoryId = $row['categoryId'];
 
         }
 
@@ -130,19 +163,21 @@ if (isset($_POST["subSubject"])) {
     while (($data = fgetcsv($file)) !== FALSE) {
         $subjectNameTh = $data[0];
         $subjectNameEng = $data[1];
-        $subjectAreasNameTh  = $data[2];
+        $subjectCredit = $data[2];
+        $subjectAreasNameTh = $data[3];
         //ดู nameCourseUse ว่า ตรงกับ courseID ไหน
         $sql = mysqli_query($conn, "SELECT * FROM subject_areas WHERE subjectAreasNameTh= '$subjectAreasNameTh'");
-        if($row = mysqli_fetch_array($sql)) {
-            $subjectAreasId   = $row['subjectAreasId'];
+        if ($row = mysqli_fetch_array($sql)) {
+            $subjectAreasId = $row['subjectAreasId'];
 
         }
 
         // เตรียม SQL statement
         $sql = "INSERT INTO subject (subjectNameTh,
                                         subjectNameEng,
+                                        subjectCredit,
                                         subjectAreasId  ) 
-        VALUES ('$subjectNameTh', '$subjectNameEng','$subjectAreasId  ')";
+        VALUES ('$subjectNameTh', '$subjectNameEng','$subjectCredit','$subjectAreasId  ')";
 
         // ดำเนินการ SQL statement
         if ($conn->query($sql) === FALSE) {
@@ -152,6 +187,19 @@ if (isset($_POST["subSubject"])) {
     // ปิดไฟล์
     fclose($file);
 }
+
+if (isset($_POST["delete"])) {
+
+    // เตรียม SQL statement
+    $sql = "DELETE FROM department";
+
+    // ดำเนินการ SQL statement
+    if ($conn->query($sql) === FALSE) {
+        echo "Error: " . $conn->error . "<br>";
+    }
+
+}
+
 
 
 // ปิดการเชื่อมต่อ MySQL
@@ -168,42 +216,30 @@ $conn->close();
 </head>
 
 <body>
-    <form action="" method="post" enctype="multipart/form-data" name="formCourse">
+    <form action="" method="post" enctype="multipart/form-data" name="formDepartment">
+        <h3>Department</h3>
+        <input name="csvDepartment" type="file" id="csvDepartment" accept=".csv">
+        <button type="submit" name="subDepartment">submit</button>
+        <br>
         <h3>course</h3>
         <input name="csvCourse" type="file" id="csvCourse" accept=".csv">
         <button type="submit" name="subCourse">submit</button>
-    </form><br>
-    <form action="" method="post" enctype="multipart/form-data" name="formCategory">
+        <br>
         <h3>category</h3>
         <input name="csvCategory" type="file" id="csvCategory" accept=".csv">
         <button type="submit" name="subCategory">submit</button>
-    </form>
-    <form action="" method="post" enctype="multipart/form-data" name="formAreas">
+        <br>
         <h3>Areas</h3>
         <input name="csvAreas" type="file" id="csvAreas" accept=".csv">
         <button type="submit" name="subAreas">submit</button>
-    </form>
-    <form action="" method="post" enctype="multipart/form-data" name="formSubject">
-        <h3>Areas</h3>
+        <br>
+        <h3>Subject</h3>
         <input name="csvSubject" type="file" id="csvSubject" accept=".csv">
         <button type="submit" name="subSubject">submit</button>
+        <br><br>
+        <button type="submit" name="delete">ลบข้อมูลทั้งหมด</button>
     </form>
-    <form action = "course.php">
-        <br><br><button name = course >Addcourse</button>
 
-    </form>
-    <form action = "category.php">
-        <br><br><button name = course >Addcategory</button>
-
-    </form>
-    <form action = "subject_areas.php">
-        <br><br><button name = course >Addsubject_areas</button>
-
-    </form>
-    <form action = "subject.php">
-        <br><br><button name = course >Addsubject</button>
-
-    </form>
 </body>
 
 </html>
